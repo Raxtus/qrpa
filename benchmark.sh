5082e098e65a90c8c14b26ee88f31b1bd1ed58db
Raxtus <46820727+Raxtus@users.noreply.github.com>
2026-06-05 11:05:00 +0200

    qasm 2

--- benchmark.sh
+++ benchmark.sh
@@ -10,7 +10,7 @@ for alg in "${algorithms[@]}"; do
     for qbit_num in {2..20}; do
 	formated_num=$(printf "%02d" "$qbit_num")
         output_file="$output_dir/${alg}_${formated_num}.qasm"
-        echo "Running: mqt-bench --level alg --algorithm $alg --num-qubits $qbit_num --output-format qasm3 > $output_file"
+        echo "Running: mqt-bench --level alg --algorithm $alg --num-qubits $qbit_num --output-format qasm2 > $output_file"
         # command
         mqt-bench --level alg --algorithm "$alg" --num-qubits "$qbit_num" --output-format qasm3 > "$output_file" 2>&1
         if [ $? -eq 0 ]; then
