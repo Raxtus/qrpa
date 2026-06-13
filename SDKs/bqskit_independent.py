e13a7e1a26968fd6e86d08e370e871ca421d10df
Raxtus <46820727+Raxtus@users.noreply.github.com>
2026-06-13 13:44:08 +0200

    bqskit debug num worker 1

--- SDKs/bqskit_independent.py
+++ SDKs/bqskit_independent.py
@@ -24,7 +24,7 @@ class BQSKitTranspilerTestSuite(QuantumTranspilerTestSuite):
         return OPENQASM2Language().decode(qasm_code)
 
     def transpile(self, circuit):
-            return compile(circuit, model=None, with_mapping=False, max_synthesis_size=4)
+            return compile(circuit, model=None, with_mapping=False, max_synthesis_size=4,num_workers=1)
 
     def verify_circuit(self, original,
                        transpiled) -> EquivalenceCheckingManager.Results:
