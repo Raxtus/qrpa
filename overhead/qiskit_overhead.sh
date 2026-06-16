23ff8373fe0497759f239d229d7caf0dc93dc801
Raxtus <46820727+Raxtus@users.noreply.github.com>
2026-06-16 19:05:55 +0200

    analysis qiskit_overhead

--- overhead/qiskit_overhead.sh
+++ overhead/qiskit_overhead.sh
@@ -4,15 +4,13 @@ sum1=0
 sum2=0
 
 for i in {1..10}; do
-result=$(python3 benchmark.py)
+result=$(python benchmark.py)
 
-```
 d1=$(echo "$result" | cut -d';' -f1)
 d2=$(echo "$result" | cut -d';' -f2)
 
 sum1=$(awk -v a="$sum1" -v b="$d1" 'BEGIN {print a+b}')
 sum2=$(awk -v a="$sum2" -v b="$d2" 'BEGIN {print a+b}')
-```
 
 done
 
