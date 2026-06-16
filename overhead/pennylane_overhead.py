8952b66fb4acdd3ba4bc97c1014a4010304cee37
Raxtus <46820727+Raxtus@users.noreply.github.com>
2026-06-16 19:22:14 +0200

    analysis qiskit_overhead

---
+++ overhead/pennylane_overhead.py
@@ -0,0 +1,16 @@
+import time
+import pennylane as qp
+
+
+qc = qp.QuantumScript()
+start_time = time.time()
+qp.compile(qc)
+end_time = time.time()
+duration_ms_1 = (end_time - start_time) * 1000
+
+
+start_time = time.time()
+qp.compile(qc)
+end_time = time.time()
+duration_ms_2 = (end_time - start_time) * 1000
+print(f"{duration_ms_1};{duration_ms_2}")
