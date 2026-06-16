f2670cdd8fcd62e571c6a2a4b93a471f09f264b9
Raxtus <46820727+Raxtus@users.noreply.github.com>
2026-06-16 19:02:25 +0200

    analysis qiskit_overhead

--- overhead/qiskit_overhead.py
+++ overhead/qiskit_overhead.py
@@ -1,17 +1,17 @@
 import time
 import qiskit
+from Cython.Shadow import returns
 from qiskit import QuantumCircuit
 
 qc = QuantumCircuit()
 start_time = time.time()
 qiskit.transpile(qc)
 end_time = time.time()
-duration_ms = (end_time - start_time) * 1000
+duration_ms_1 = (end_time - start_time) * 1000
 
-print(duration_ms)
 
 start_time = time.time()
 qiskit.transpile(qc)
 end_time = time.time()
-duration_ms = (end_time - start_time) * 1000
-print(duration_ms)
+duration_ms_2 = (end_time - start_time) * 1000
+print(f"{duration_ms_1};{duration_ms_2}")
--- overhead/qiskit_overhead.sh
+++ overhead/qiskit_overhead.sh
@@ -1,5 +1,23 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
-for i in in {1..10}; do
-  python qiskit_overhead.py
+sum1=0
+sum2=0
+
+for i in {1..10}; do
+result=$(python3 benchmark.py)
+
+```
+d1=$(echo "$result" | cut -d';' -f1)
+d2=$(echo "$result" | cut -d';' -f2)
+
+sum1=$(awk -v a="$sum1" -v b="$d1" 'BEGIN {print a+b}')
+sum2=$(awk -v a="$sum2" -v b="$d2" 'BEGIN {print a+b}')
+```
+
 done
+
+avg1=$(awk -v s="$sum1" 'BEGIN {print s/10}')
+avg2=$(awk -v s="$sum2" 'BEGIN {print s/10}')
+
+echo "Average first transpile:  $avg1 ms"
+echo "Average second transpile: $avg2 ms"
